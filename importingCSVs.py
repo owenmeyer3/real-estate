c78580655979db59060d18a4c4b541a575108b4d
sambarton10 <69605114+sambarton10@users.noreply.github.com>
2020-10-27 20:41:40 -0500

    Phase1 Import Data

---
+++ importingCSVs.py
@@ -0,0 +1,11 @@
+
+import pandas as pd
+import numpy as np
+
+cityCodes = pd.read_csv('FMCityCodes.csv')
+
+team = pd.read_csv('team.csv')
+
+city_mergeDF = pd.merge(team, cityCodes, how = 'left', on='city_string')
+print(city_mergeDF.columns)
+
