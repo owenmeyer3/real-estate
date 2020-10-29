528ae8d74a5c87111b2d18d0ff4b58327071d4f6
owenmeyer3 <owenmeyer3@gmail.com>
2020-10-29 15:59:24 -0500

    added t value to graphs. recolored. deleted stats file as the stats are in the plot loop

--- plot.py
+++ plot.py
@@ -1,9 +1,10 @@
 from importingCSVs import cityCodes
 from getCityData import indicesDict, areaCodes, USIndexDF
 import matplotlib.pyplot as plt
+from scipy import stats
 #import pandas as pd
 
-saDiffDict = {}
+# unneeded saDiffDict = {}
 
 ####plot date vs. sa (city, US and city - US) for select city
 #Choose city
@@ -16,22 +17,31 @@ for areaCode in areaCodes:
     cityUSDF = plotDF.merge(USIndexDF, on='date')
     cityUSDF['sa'] = cityUSDF['sa_x'] - cityUSDF['sa_y']
     cityUSDF.rename(columns = {'sa_x':'sa_city', 'sa_y':'sa_US', 'sa':'sa_city-US'}, inplace = True)
-    saDiffDict[str(areaCode)]=cityUSDF[['date', 'sa_city-US']]
+    #Unneeded - saDiffDict[str(areaCode)]=cityUSDF[['date', 'sa_city-US']]
+
+    #find 0.95 sample limit
+    mean = cityUSDF['sa_city-US'].mean()
+    std = cityUSDF['sa_city-US'].std()
+    t_95 = stats.norm.ppf(0.95, loc = 0, scale = 1)
+    x_95 = t_95*std + mean
+    x_min = cityUSDF['date'].min()
+    x_max = cityUSDF['date'].max()
+    plt.hlines(x_95, x_min, x_max, 'k', label = '0.95 significance limit')
 
     #set x, y arrays and plot for city
     x1 = cityUSDF['date'].values
     y1 = cityUSDF['sa_city'].values
-    plt.plot(x1,y1, label = cityName)
+    plt.plot(x1,y1, 'b', label = cityName)
 
     #set x, y arrays and plot for US
     x2 = cityUSDF['date'].values
     y2 = cityUSDF['sa_US'].values
-    plt.plot(x2,y2, label = 'US')
+    plt.plot(x2,y2, 'r', label = 'US')
 
     #set x, y arrays and plot for city - US
     x3 = cityUSDF['date'].values
     y3 = cityUSDF['sa_city-US'].values
-    plt.plot(x3,y3, label = 'difference ( city - US)')
+    plt.plot(x3,y3, 'g', label = 'difference ( city - US)')
 
     #set labels
     plt.title(cityName)
